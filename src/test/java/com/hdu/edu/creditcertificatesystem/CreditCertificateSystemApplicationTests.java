e65de71463900d1a1e8cecd80b123848e8c4ae84
chenyb46701 <Cyb1255210!@>
2023-03-12 21:11:05 +0800

    初始化项目结构

---
+++ src/test/java/com/hdu/edu/creditcertificatesystem/CreditCertificateSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hdu.edu.creditcertificatesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditCertificateSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
