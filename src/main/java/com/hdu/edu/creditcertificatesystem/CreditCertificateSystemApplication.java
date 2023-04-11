5a633dc23e4c43d28c775e410273b67988fa4df4
chenyb46701 <Cyb1255210!@>
2023-04-11 15:05:19 +0800

    1、修改系统入口，删除mapperScan注释

--- src/main/java/com/hdu/edu/creditcertificatesystem/CreditCertificateSystemApplication.java
+++ src/main/java/com/hdu/edu/creditcertificatesystem/CreditCertificateSystemApplication.java
@@ -14,7 +14,6 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
  */
 @SpringBootApplication
 @ConfigurationPropertiesScan
-@MapperScan("com.hdu.edu.creditcertificatesystem.mapper")
 public class CreditCertificateSystemApplication {
 
     public static void main(String[] args) {
