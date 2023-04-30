3b6642555c9bfbb84f61eff0bbfb78b28988ed31
chenyb46701 <Cyb1255210!@>
2023-04-30 21:31:27 +0800

    1、增加角色权限枚举

---
+++ src/main/java/com/hdu/edu/creditcertificatesystem/enums/RolePermissionEnum.java
@@ -0,0 +1,60 @@
+package com.hdu.edu.creditcertificatesystem.enums;
+
+import lombok.Getter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 角色权限枚举
+ *
+ * @author chenyb46701
+ * @date 2023/4/23
+ */
+@Getter
+public enum RolePermissionEnum {
+    /**
+     * 系统管理员
+     */
+    ADMIN(0, "admin"),
+
+    /**
+     * 教务管理员
+     */
+    EDUCATIONAL_MANAGER(1, "educational_manager"),
+
+    /**
+     * 机构审核员
+     */
+    INSTITUTE_MANAGER(2, "institute_manager"),
+
+    /**
+     * 老师
+     */
+    TEACHER(3, "teacher"),
+
+    /**
+     * 学生
+     */
+    STUDENT(4, "student");
+
+    private static final Map<String, ContractTypeEnum> KEY_MAP = new HashMap<>(8);
+
+    static {
+        for (ContractTypeEnum contractTypeEnum : ContractTypeEnum.values()) {
+            KEY_MAP.put(contractTypeEnum.getKey(), contractTypeEnum);
+        }
+    }
+
+    private final Integer key;
+
+    private final String value;
+    RolePermissionEnum(Integer key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public static ContractTypeEnum getByKey(String key) {
+        return KEY_MAP.get(key);
+    }
+}
