73ab3694f07ad9e1c24637f4d356d9294fd8d3ab
chenyb46701 <Cyb1255210!@>
2023-05-02 21:20:04 +0800

    1、excel工具类实现

---
+++ src/main/java/com/hdu/edu/creditcertificatesystem/util/ExcelUtils.java
@@ -0,0 +1,123 @@
+package com.hdu.edu.creditcertificatesystem.util;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.poi.ss.usermodel.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * excel工具类
+ *
+ * @author chenyb46701
+ * @date 2023/3/13
+ */
+@Slf4j
+public class ExcelUtils {
+
+    public static InputStream getResourcesFileInputStream(String fileName) {
+        return Thread.currentThread().getContextClassLoader().getResourceAsStream("" + fileName);
+    }
+
+    public static String getPath() {
+        return ExcelUtils.class.getResource("/").getPath();
+    }
+
+    public static File createNewFile(String pathName) {
+        File file = new File(getPath() + pathName);
+        if (file.exists()) {
+            file.delete();
+        } else {
+            if (!file.getParentFile().exists()) {
+                file.getParentFile().mkdirs();
+            }
+        }
+        return file;
+    }
+
+    public static File readFile(String pathName) {
+        return new File(getPath() + pathName);
+    }
+
+    public static File readUserHomeFile(String pathName) {
+        return new File(System.getProperty("user.home") + File.separator + pathName);
+    }
+
+    /**
+     * POI解析excel文件
+     *
+     * @param file
+     * @return
+     */
+    public static List<Map<Integer, List<String>>> readExcel(MultipartFile file) {
+        Workbook workbook = null;
+        List<Map<Integer, List<String>>> totalList = new ArrayList<>();
+        try {
+            workbook = WorkbookFactory.create(file.getInputStream());
+            // 获取sheet数量
+            int sheetNumber = workbook.getNumberOfSheets();
+            log.info("共有{}个sheet页", sheetNumber);
+            for (int i = 0; i < sheetNumber; i++) {
+                // 存储单页sheet信息map
+                Map<Integer, List<String>> map = new HashMap<>();
+
+                // 获取当前sheet
+                Sheet sheet = workbook.getSheetAt(i);
+                int rowNum = sheet.getLastRowNum();
+                log.info("共有{}行数据", rowNum);
+
+                for (int k = 1; k <= rowNum; k++) {
+                    List<String> infoList = new ArrayList<>();
+                    Row rowData = sheet.getRow(k);
+                    if (rowData != null) {
+                        // 读取列
+                        int cellCount = rowData.getLastCellNum();
+                        for (int cellNum = 0; cellNum < cellCount; cellNum++) {
+                            Cell cell = rowData.getCell(cellNum);
+                            // 匹配数据类型
+                            String cellValue = "";
+                            if (cell != null) {
+                                if (cell.getCellTypeEnum() == CellType.STRING) {
+                                    cellValue = cell.getStringCellValue();
+                                }
+
+                                if (cell.getCellTypeEnum() == CellType.NUMERIC) {
+                                    //数值型
+                                    //poi读取整数会自动转化成小数，这里对整数进行还原，小数不做处理
+                                    long longValue = Math.round(cell.getNumericCellValue());
+                                    if (Double.parseDouble(longValue + ".0") == cell.getNumericCellValue()) {
+                                        cellValue = String.valueOf(longValue);
+                                    } else {
+                                        cellValue = String.valueOf(cell.getNumericCellValue());
+                                    }
+                                }
+                                infoList.add(cellValue);
+                            } else {
+                                infoList.add(null);
+                            }
+                        }
+                        map.put(k, infoList);
+                    }
+                }
+                totalList.add(map);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (workbook != null) {
+                    workbook.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return totalList;
+    }
+}
