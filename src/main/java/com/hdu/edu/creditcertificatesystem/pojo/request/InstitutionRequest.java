d2c4b873e804da0540acb8d0e81bc3a8873fea3d
chenyb46701 <Cyb1255210!@>
2023-05-04 09:59:25 +0800

    1、增加机构信息审核相关字段

--- src/main/java/com/hdu/edu/creditcertificatesystem/pojo/request/InstitutionRequest.java
+++ src/main/java/com/hdu/edu/creditcertificatesystem/pojo/request/InstitutionRequest.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.math.BigInteger;
+
 /**
  * 机构信息请求类
  *
@@ -13,6 +15,69 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
-public class InstitutionRequest {
+public class InstitutionRequest extends BaseRequest {
+    /**
+     * 主键Id
+     */
+    private String id;
 
+    /**
+     * 账号
+     */
+    private String account;
+
+    /**
+     * 密码
+     */
+    private String password;
+
+    /**
+     * 机构名
+     */
+    private String institutionName;
+
+    /**
+     * 机构电话号码
+     */
+    private String institutionPhone;
+
+    /**
+     * 机构邮箱
+     */
+    private String institutionEmail;
+
+    /**
+     * 机构地址
+     */
+    private String institutionPlace;
+
+    /**
+     * 审核状态；0待审核；1已通过；
+     */
+    private BigInteger status;
+
+    /**
+     * 授权证明照片
+     */
+    private String authorCertificatePic;
+
+    /**
+     * 创建时间
+     */
+    private String createTime;
+
+    /**
+     * 审核时间
+     */
+    private String auditTime;
+
+    /**
+     * 留言
+     */
+    private String message;
+
+    /**
+     * 驳回原因
+     */
+    private String reason;
 }
